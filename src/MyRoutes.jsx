a4e2ca891b7be45e29140e10b8956f27f837f6f4
kishor munikar <k1shor@hotmail.com>
2026-03-10 13:57:37 +0545

    routing fixed

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Homepage from './pages/Homepage'
 import CartPage from './pages/CartPage'
 
 const MyRoutes = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route element={<Layout />}>
                     <Route index element={<Homepage />} />
-                    <Route path='cart' element = {<CartPage/>}/>
+                    <Route path='cart' element={<CartPage />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
